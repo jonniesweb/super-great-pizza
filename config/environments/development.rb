9c926a581fbdb9ad63619792d0d5923bb123b814
Jon Simpson <jon.simpson@shopify.com>
2019-05-07 20:51:31 -0400

    allow local development with dev

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -52,6 +52,9 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  # allow access for local development with Dev
+  config.hosts << "super-great-pizza.myshopify.io"
+
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
